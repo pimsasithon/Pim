2686648b689653cd2f5b597045a7df2002bbb749
pimsasithon <sasithon.su@ku.th>
2023-09-18 15:07:54 +0700

    add file

---
+++ lib/screen/show_data.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Show_data extends StatefulWidget {
+  const Show_data({super.key});
+
+  @override
+  State<Show_data> createState() => _Show_dataState();
+}
+
+class _Show_dataState extends State<Show_data> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

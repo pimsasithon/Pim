ce1cedd11a773aeed04e62cb8849e316a966e1a5
pimsasithon <sasithon.su@ku.th>
2023-09-18 15:05:24 +0700

    commit

--- lib/screen/display.dart
+++ lib/screen/display.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class Check_symptom extends StatefulWidget {
+  const Check_symptom({super.key});
+
+  @override
+  State<Check_symptom> createState() => _Check_symptomState();
+}
+
+class _Check_symptomState extends State<Check_symptom> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
